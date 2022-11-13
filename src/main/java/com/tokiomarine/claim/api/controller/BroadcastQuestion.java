22217665522c5f7a09e9f0bee3d5f2123c13f65b
Pabwai <117081869+Pabwai@users.noreply.github.com>
2022-11-13 23:54:33 +0700

    add token

--- src/main/java/com/tokiomarine/claim/api/controller/BroadcastQuestion.java
+++ src/main/java/com/tokiomarine/claim/api/controller/BroadcastQuestion.java
@@ -5,15 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.tokiomarine.claim.api.config.Thumbprint;
@@ -29,9 +28,7 @@ public class BroadcastQuestion {
     String ApiBroadcastOffline = "https://api-test.insure.co.th/QuestionService/OfflineBroadcastQuestion/";
     String ApiAnsCompanyPublicKey = "https://api-test.insure.co.th/PKI/PublicKey";
     
-	public String SendingQuestion(GetGateWayRequest data ) throws IOException{
-		
-		
+	public String SendingQuestion(GetGateWayRequest data ) throws IOException{		
 		
 		try {
 			Thumbprint.disableSSLVerificationForHttps();
@@ -83,6 +80,64 @@ public class BroadcastQuestion {
 			System.out.println("POST request not worked");
 			return "POST request not worked";
 		}
+	}
+	
+	public String getCondition(GetGateWayRequest data,String token ) throws MalformedURLException, IOException {
+		
+		try {
+			Thumbprint.disableSSLVerificationForHttps();
+		} catch (KeyManagementException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
+		Map<String, String> map = new HashMap<>();
+		map.put("qusCompany", data.getQusCompany());
+		map.put("ansCompany", data.getAnsCompany());
+		map.put("ServiceCode", data.getServiceCode().toCharArray().toString());
+		map.put("ServiceCondition", data.getCompanyCode());
+		map.put("RequestDatetime", data.getCompanyCode());
+		map.put("UserRequest", data.getUsername());
+		JSONObject users = new JSONObject(map);
+		
+		HttpURLConnection con = (HttpURLConnection) new URL(ApiBroadcastOffline).openConnection();
+		con.setRequestMethod("POST");
+		con.setRequestProperty("Authorization","Bearer " + token);
+		con.setRequestProperty("Content-Type","application/json");
+		
+		// For POST only - START
+		con.setDoOutput(true);
+		OutputStream os = con.getOutputStream();
+		os.write(users.toString().getBytes());
+		os.flush();
+		os.close();
+		// For POST only - END
+
+		int responseCode = con.getResponseCode();
+		System.out.println("POST Response Code :: " + responseCode);
+
+		if (responseCode == HttpURLConnection.HTTP_OK) { //success
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			// print result
+			System.out.println(response.toString());
+			return response.toString();
+		} else {
+			System.out.println("POST request not worked");
+			return "POST request not worked";
+		}	
+		
 	}
 	
 
--- src/main/java/com/tokiomarine/claim/api/controller/WebServiceEndpiont.java
+++ src/main/java/com/tokiomarine/claim/api/controller/WebServiceEndpiont.java
@@ -2,13 +2,14 @@ package com.tokiomarine.claim.api.controller;
 
 import java.io.IOException;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
-
+import tokiomarine.service.gen.Condition;
 import tokiomarine.service.gen.GetGateWayRequest;
 import tokiomarine.service.gen.GetGateWayResponse;
 
@@ -16,20 +17,20 @@ import tokiomarine.service.gen.GetGateWayResponse;
 public class WebServiceEndpiont {
 	
 	@Autowired
-	BroadcastQuestion broadcastQuestion;	
-	
+	BroadcastQuestion broadcastQuestion;
+
 	private static final String NAMESPACE_URI = "http://tokiomarine/service/gen";
 
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getGateWayRequest")
 	@ResponsePayload
 	public GetGateWayResponse getCountry(@RequestPayload GetGateWayRequest request) throws IOException {
 		GetGateWayResponse response = new GetGateWayResponse();	
-		
-		String token = broadcastQuestion.SendingQuestion(request);		
-		//CliamsKnockReqModel s = new CliamsKnockReqModel();
-		
-		//System.out.println(s.toString());		
-		response.setTx(token);
+		Condition s = new Condition();
+		//String token = broadcastQuestion.SendingQuestion(request);	
+		System.out.println(request.getServiceCondition().toString());
+		JSONObject users = new JSONObject(request.getServiceCondition().toString());
+		System.out.println(users.toString());
+		response.setQusCompany("2");
 		return response;
 	}
 
